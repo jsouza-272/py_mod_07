b9d1667c0918c2faccf8e1b691f6331817d73bb1
Joao Souza <jsouza@c1r5s9.42lisboa.com>
2026-03-02 10:13:22 +0000

    end

--- __init__.py
+++ __init__.py
@@ -1,14 +1,16 @@
-from .ex0 import CreatureCard
+from .ex0 import Card, CreatureCard
 from .ex1 import SpellCard, ArtifactCard, Deck
 from .ex2 import EliteCard, Combatable, Magical
 from .ex3 import GameEngine, AggressiveStrategy, GameStrategy
 from .ex3 import CardFactory, FantasyCardFactory
+from .ex4 import TournamentCard, Rankable, TournamentPlataform
 
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __author__ = "jsouza"
-__all__ = [CreatureCard,
+__all__ = [Card, CreatureCard,
            SpellCard, ArtifactCard, Deck,
            EliteCard, Combatable, Magical,
            GameEngine, AggressiveStrategy, GameStrategy,
-           CardFactory, FantasyCardFactory]
+           CardFactory, FantasyCardFactory,
+           TournamentCard, Rankable, TournamentPlataform]
